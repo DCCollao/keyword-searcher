56436907695bf3a47314c8289298b6f6ace5685d
DCCollao <dlcollao@uc.cl>
2020-10-02 20:24:45 -0300

    Added comment to flag lines to fix due to bad code :poop:

--- alerta.py
+++ alerta.py
@@ -5,53 +5,52 @@ import sys
 
 
 
-if __name__ == "__main__":
-    
-    SEARCH = ["md"]  # Agregar extensiones de archivo a revisar por el programa
-    # Ojo con agregar "py" y revisar las tareas 3. Pasa que algunos alumnos
-    # suben archivos autogenerados de literalmente millones de líneas.
-    nombre_carpeta_revisada = sys.argv[1]
+SEARCH = ["md"]  # Agregar extensiones de archivo a revisar por el programa
+# Ojo con agregar "py" y revisar las tareas 3. Pasa que algunos alumnos
+# suben archivos autogenerados de literalmente millones de líneas.
 
-    with open("keywords.json") as file:
-        KEYWORDS = json.load(file)["keywords"] 
+# Corregir programa para no pedir esto por consola
+nombre_carpeta_revisada = sys.argv[1]
 
-    root_dir = os.getcwd()
+with open("keywords.json") as file:
+    KEYWORDS = json.load(file)["keywords"] 
 
-    usernames = list()
-    for root, dirs, files in os.walk(root_dir, onerror=None):
-        #Itera sobre todas las carpetas, subcarpetas y archivos
-        for filename in files:
-            file_path = os.path.join(root, filename)
-            lines = list()
-            extension = file_path.split(os.sep)[-1].split(".")[-1] #obtiene la extensión del archivo
-            if extension in SEARCH:
-                #Revisamos solamente las extensiones que declaramos en la lista SEARCH, línea 10.
-                try:
-                    with open(file_path, "r") as f:
-                        for line in f.readlines():
-                            lines.append(line.strip().split(" "))
-                        lines = reduce(lambda x, y: x + y, lines, [])
-                        intersection = [_ for _ in lines if _ in KEYWORDS]
-                        if intersection:
-                            #Si la lista no está vacía significa que el archivo tenía alguna keyword en él.
-                            
-                            file_path_list = file_path.split(os.sep)
-                            index = file_path_list.index(nombre_carpeta_revisada)
-                            
-                            #Como los archivos tienen ruta "/TXX/Username", buscamos index de TXX
-                            # y el elemento siguiente(index + 1) es su username. Lo agrego a una lista de
-                            # usernames "sospechosos"
-                            usernames.append(file_path_list[index + 1])
-                            
-                except (IOError, OSError, UnicodeDecodeError):  
-                    #Por si las moscas
-                    pass
-            
+root_dir = os.getcwd()
+
+usernames = list()
+for root, dirs, files in os.walk(root_dir, onerror=None):
+    #Itera sobre todas las carpetas, subcarpetas y archivos
+    for filename in files:
+        file_path = os.path.join(root, filename)
+        lines = list()
+        extension = file_path.split(os.sep)[-1].split(".")[-1] #obtiene la extensión del archivo
+        if extension in SEARCH:
+            #Revisamos solamente las extensiones que declaramos en la lista SEARCH, línea 10.
+            try:
+                with open(file_path, "r") as f:
+                    for line in f.readlines():
+                        lines.append(line.strip().split(" "))
+                    lines = reduce(lambda x, y: x + y, lines, [])
+                    intersection = [_ for _ in lines if _ in KEYWORDS]
+                    if intersection:
+                        #Si la lista no está vacía significa que el archivo tenía alguna keyword en él.
+                        
+                        file_path_list = file_path.split(os.sep)
+                        index = file_path_list.index(nombre_carpeta_revisada)
+                        
+                        #Como los archivos tienen ruta "/TXX/Username", buscamos index de TXX
+                        # y el elemento siguiente(index + 1) es su username. Lo agrego a una lista de
+                        # usernames "sospechosos"
+                        usernames.append(file_path_list[index + 1])
+                        
+            except (IOError, OSError, UnicodeDecodeError):  
+                #Por si las moscas
+                pass
 
 
-    usernames = set(usernames)
-    with open("_usernames.txt", "w") as file:
-        #Creamos el archivo _usernames.txt y lo llenamos con los usernames que nos interesan
-        for user in usernames:
-            file.writelines(user + "\n")
-    print("Encontrados", len(usernames), "casos sospechosos")
+usernames = set(usernames)
+with open("_usernames.txt", "w") as file:
+    #Creamos el archivo _usernames.txt y lo llenamos con los usernames que nos interesan
+    for user in usernames:
+        file.writelines(user + "\n")
+print("Encontrados", len(usernames), "casos sospechosos")
